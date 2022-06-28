3afae217704bd56094abf37b836f6cc3f8ef809e
SeyramWood <prikahaeyram@gmail.com>
2022-06-28 10:17:09 +0000

    .env pkg and config pkg modified

--- config/database.go
+++ config/database.go
@@ -15,13 +15,23 @@ type db struct {
 func DB(driver string) *db {
 	switch driver {
 	case "mysql":
+		if env.Get("APP_ENV", "local") == "local" {
+			return &db{
+				Driver:   "mysql",
+				Host:     env.Get("DB_HOST", "127.0.0.1"),
+				Port:     env.Get("DB_PORT", "3306"),
+				Name:     env.Get("DB_DATABASE", "test_db"),
+				Username: env.Get("DB_USERNAME", "root"),
+				Password: env.Get("DB_PASSWORD", ""),
+			}
+		}
 		return &db{
 			Driver:   "mysql",
-			Host:     env.Get("DB_HOST", "127.0.0.1"),
-			Port:     env.Get("DB_PORT", "3306"),
-			Name:     env.Get("DB_DATABASE", "test_db"),
-			Username: env.Get("DB_USERNAME", "root"),
-			Password: env.Get("DB_PASSWORD", ""),
+			Host:     env.GetProd("DB_HOST", "127.0.0.1"),
+			Port:     env.GetProd("DB_PORT", "3306"),
+			Name:     env.GetProd("DB_DATABASE", "test_db"),
+			Username: env.GetProd("DB_USERNAME", "root"),
+			Password: env.GetProd("DB_PASSWORD", ""),
 		}
 	}
 	return nil
--- pkg/env/env.go
+++ pkg/env/env.go
@@ -3,6 +3,7 @@ package env
 import "github.com/joho/godotenv"
 
 var Env map[string]string
+var EnvProd map[string]string
 
 func Get(key, def string) string {
 	if val, ok := Env[key]; ok {
@@ -11,10 +12,19 @@ func Get(key, def string) string {
 	return def
 }
 
+func GetProd(key, def string) string {
+	if val, ok := EnvProd[key]; ok {
+		return val
+	}
+	return def
+}
+
 func Setup() {
 	envFile := ".env"
+	envFileProd := ".env.production"
 	var err error
 	Env, err = godotenv.Read(envFile)
+	EnvProd, err = godotenv.Read(envFileProd)
 	if err != nil {
 		panic(err)
 	}
